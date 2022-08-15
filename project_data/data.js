843b8943bdc4b209a5e3e86638f2dc5f485f992e
nvkko <111242688+nvkko@users.noreply.github.com>
2022-08-15 18:20:54 -0400

    Add files via upload

---
+++ project_data/data.js
@@ -0,0 +1,15 @@
+const data = {};
+
+data.firstTxt = 'The Matrix has you...';
+
+data.secondTxt = 'Follow the White Path...';
+
+data.thirdTxt = '> Learn the Truth <';
+
+
+data.fontSize = 30;
+
+data.textHeight = 35;
+
+// the lower the faster
+data.typeSpeed = 2;
